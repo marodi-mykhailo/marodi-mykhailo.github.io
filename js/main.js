971cb2b2ddee4dbe9d8547d0adc5156f90ad01c6
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-05-03 14:49:38 +0200

    koniec projektu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.slider-wrapper').slick({
+        nextArrow: '<button type="button" class="slider-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slider-btn slick-prev"></button>',
+        autoplay:true,
+        autoplaySpeed: 4000,
+        mobileFirst: true,
+        focusOnSelect: true,
+        speed: 500,
+        swipeToSlide: true,
+    });
+    });
