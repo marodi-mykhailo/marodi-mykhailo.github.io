ad76f1241b8b329c63e816431e1733e1c45e0c19
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-12-09 23:05:57 +0100

    add responsive to index.html

--- js/main.js
+++ js/main.js
@@ -9,4 +9,56 @@ $(function(){
         speed: 500,
         swipeToSlide: true,
     });
+
+$('.slider2').slick({
+    nextArrow: '<button type="button" class="slider-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slider-btn slick-prev"></button>',
+    slidesToShow: 3,
+    autoplay: false,
+    arrows: false,
+    responsive: [
+        {
+          breakpoint: 1110,
+          settings: {
+            arrows: true,
+
+            slidesToShow: 2
+          }
+        },
+        {
+            breakpoint: 660,
+            settings: {
+              arrows: true,
+  
+              slidesToShow: 1
+            }
+        }
+    ]
+})
+
+    $('.slider3').slick({
+        nextArrow: '<button type="button" class="slider-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slider-btn slick-prev"></button>',
+        slidesToShow: 4,
+        autoplay: false,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1110,
+              settings: {
+                arrows: true,
+                centerMode: true,
+                slidesToShow: 2
+              }
+            },
+            {
+                breakpoint: 660,
+                settings: {
+                  arrows: true,
+      
+                  slidesToShow: 1
+                }
+            }
+        ]
+    })
     });
